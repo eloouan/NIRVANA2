6ecb7e9e284e354c5b734b804aae24a2b41dae71
MomongaAinz <fezioroff@gmail.com>
2023-12-11 21:12:01 +0100

    animation change

--- animations/index.js
+++ animations/index.js
@@ -11,42 +11,11 @@ export const preLoaderAnim = () => {
     css: { overflowY: "hidden" },
     ease: "power3.inOut",
   })
-    .to(".landing", {
-      duration: 0.05,
-      css: { overflowY: "hidden", height: "90vh" },
-    })
-    .to(".texts-container", {
-      duration: 0,
-      opacity: 1,
-      ease: "Power3.easeOut",
-    })
-    .from(".texts-container span", {
-      duration: 1.5,
-      delay: 1,
-      y: 70,
-      skewY: 10,
-      stagger: 0.4,
-      ease: "Power3.easeOut",
-    })
-    .to(".texts-container span", {
-      duration: 1,
-      y: 70,
-      skewY: -20,
-      stagger: 0.2,
-      ease: "Power3.easeOut",
-    })
-
-    .to(".landing", {
-      duration: 0.05,
-      css: { overflowY: "hidden", height: "unset" },
-    })
-    .to("body", {
-      duration: 0.1,
-      css: { overflowY: "scroll" },
-      ease: "power3.inOut",
-    })
-    .from(".landing__top .sub", {
-      duration: 1.5,///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    
+      
+    .from(".cette_erreur_est_normal", {
+      duration: 6,///////////////////////////////////////////////////////////////////////////////////////////////////////////
       opacity: 0,
       y: 80,
       ease: "expo.easeOut",
@@ -61,40 +30,10 @@ export const preLoaderAnim = () => {
       },
       "-=2"
     )
-    .from(".landing__main .text", {
-      duration: 2,
-      // scale: 0,
-      y: 10,
-      opacity: 0,
-      stagger: {
-        amount: 2,
-      },
-      ease: "power3.easeInOut",
-    })
-    .from(".links .item", {
-      duration: 0.5,
-      opacity: 0,
-      delay: window.innerWidth < 763 ? -3 : -0.6,
-      // y: 80,
-      stagger: {
-        amount: 0.5,
-      },
-      ease: "expo.easeOut",
-      onComplete: animateMainShape(),
-    })
-    .from(".main-circle", {
-      duration: 1,
-      opacity: 0,
-      ease: "power3.easeInOut",
-      onComplete: animateShapes(),
-    })
-    .from(".shapes .shape", {
-      duration: 1,
-      opacity: 0,
-      delay: -1,
-      ease: "power3.easeInOut",
-      stagger: 1,
-    })
+    
+    
+    
+    
     .to(".preloader", {
       duration: 0,
       css: { display: "none" },
@@ -217,38 +156,7 @@ const animateShapes = () => {
     repeat: -1,
   });
   infiniteTl
-    .to(".shapes .shape", {
-      duration: 4,
-      rotate: 360,
-      delay: -1,
-      ease: "power3.easeInOut",
-      stagger: 2,
-    })
-    .to(".shapes .shape-3", {
-      duration: 1,
-      rotate: 360,
-      delay: -2,
-      ease: "power3.easeInOut",
-    })
-    .to(".shapes .shape", {
-      duration: 3,
-      rotate: 0,
-      ease: "power3.easeInOut",
-      stagger: 1,
-    })
-    .to(".shapes .shape", {
-      duration: 1,
-      opacity: 0,
-      delay: -1,
-      ease: "power3.easeInOut",
-      stagger: 1,
-    })
-    .to(".shapes .shape", {
-      duration: 1.5,
-      opacity: 1,
-      ease: "power3.easeInOut",
-      stagger: 1,
-    });
+    
 };
 
 const animateMainShape = () => {
@@ -256,24 +164,7 @@ const animateMainShape = () => {
     repeat: -1,
   });
   infiniteTl
-    .to(".shapes .main-circle", {
-      duration: 6,
-      x: -30,
-      y: -50,
-      ease: "expo.easeOut",
-    })
-    .to(".shapes .main-circle", {
-      duration: 6,
-      x: -30,
-      y: 50,
-      ease: "expo.easeOut",
-    })
-    .to(".shapes .main-circle", {
-      duration: 4,
-      x: 0,
-      y: 0,
-      ease: "expo.easeOut",
-    });
+    
 };
 
 export const boxHover = (e) => {
